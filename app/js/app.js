9896686420c1b30d36b9ab513a58d6189599b148
Avraham <ring4@inbox.ru>
2017-06-05 14:27:11 +0300

    removed trash

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,10 @@
 import Location from './location';
 
-function base64ToArrayBuffer(base64) {
-  var binary_string =  window.atob(base64);
-  var len = binary_string.length;
-  var bytes = new Uint8Array( len );
-  for (var i = 0; i < len; i++)        {
-    bytes[i] = binary_string.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
-
 class App {
   constructor() {
     this.init();
     this.initHandlers();
   }
-
-  XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
-  xhr = new this.XHR();
 
   markersList = {};
   DELAY = 175;
